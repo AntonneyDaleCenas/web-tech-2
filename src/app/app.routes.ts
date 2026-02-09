b6dc04da8dccb134b2678f57d62e66454a4bc103
Antonney Cenas <acenas42729@liceo.edu.ph>
2026-02-09 11:16:54 +0800

    Prelim Exam

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { StudentsComponent } from './students/students.component';
 import { CreateStudentComponent } from './create-students/create-student.component';
+import { PrelimExamComponent } from './prelim-exam/prelim-exam.component';
 
 export const routes: Routes = [
   { path: 'students', component: StudentsComponent },
   { path: 'create-student', component: CreateStudentComponent },
-  { path: '', redirectTo: 'students', pathMatch: 'full' }
+  { path: '', redirectTo: 'students', pathMatch: 'full' },
+  { path: 'prelim-exam', component: PrelimExamComponent }
 ];
--- src/prelim-exam/prelim-exam.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, signal} from "@angular/core";
-
-@Component({
-    selector: 'app-prelim-exam',
-    templateUrl:'./prelim-exam.component.html',
-    styleUrls: ['./prelim-exam.component.scss']
-})
-export class PrelimExamComponent{
-}
